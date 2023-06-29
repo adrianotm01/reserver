7c1df6ee6c0e3caca799aaf0a80a535c8e7ce08b
Adriano Junior <adriano.junior@senior.com.br>
2023-06-29 09:03:49 -0300

    Adiciona estrutura do projeto

---
+++ src/test/java/br/com/mand/reserver/ReserverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mand.reserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
